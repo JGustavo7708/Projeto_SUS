ae4c1c7d97332040a64f65b72908cd673ea7bae7
JGustavo7708 <guugs20i15@gmail.com>
2025-03-31 19:58:24 -0300

    Update cadastro.py
    adicionei os dados pessoais

--- cadastro.py
+++ cadastro.py
@@ -5,3 +5,14 @@ class usuario():
     def cadastro(self):
         self.nome = str(input("Digite o seu nome: "))    
         self.email = str(iput("Digite o seu email: "))
+
+dados ={'Pedro': '123.456.789-00',
+        'Lucas': '123.456.789-01',
+        'Ana': '123.456.789-02',
+        'Marcos': '123.456.789-03',
+        'Fernanda': '123.456.789-04',
+        'Roberta': '123.456.789-05',
+        'Carlos': '123.456.789-06',
+        'Alice': '123.456.789-07',
+        "Juliana": '123.456.789-08'}
+
