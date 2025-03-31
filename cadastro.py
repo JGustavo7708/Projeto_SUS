06de6bb83887c4208cbad8fdd4c5fc56f36dd8c5
lucaspy-stack <lucaswork.ps@gmail.com>
2025-03-31 19:14:00 -0300

    inicio do sistema de cadastro

---
+++ cadastro.py
@@ -0,0 +1,7 @@
+class usuario():
+    def __init__(self):
+        self.cadastro()
+
+    def cadastro(self):
+        self.nome = str(input("Digite o seu nome: "))    
+        self.email = str(iput("Digite o seu email: "))
